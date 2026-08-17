51a69385b006d7907011e2b8bcd99e8f255a849f
kaseywright <kasey.wright@biblionexus.org>
2026-08-17 10:33:17 -0400

    revert unnecessary role change

--- src/lib/types/base.ts
+++ src/lib/types/base.ts
@@ -118,7 +118,7 @@ export interface User extends BasicUser {
 
 export interface CurrentUser extends BasicUser {
     permissions: Permission[];
-    role: UserRole;
+    roles: UserRole[];
     company: { id: number; languageIds: number[] };
     languageId: number | null;
     canBeAssignedContent: boolean;
